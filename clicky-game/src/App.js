2ba710314516c18e6f8678fad4fbaf6dd18c23cc
jessicacord <jessicaleecord@gmail.com>
2018-05-05 10:10:18 -0400

    game finished in one build. whoops

---
+++ clicky-game/src/App.js
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import TopBar from "./components/TopBar";
+import Banner from "./components/Banner";
+import GameBoard from "./components/GameBoard";
+import Card from "./components/Card";
+import characters from "./characters.json"
+import './App.css';
+
+
+class App extends Component {
+  state = {
+    characters: characters,
+    clicked: [],
+    score: 0,
+    topScore: 0,
+    message: ""
+  }
+
+  randomizeCharacters = () => {
+    characters.sort((a,b) => { return 0.5 - Math.random()});
+    this.setState({characters: characters});
+  }
+
+  clickedCard = id => {
+    const clicked = this.state.clicked;
+    clicked.push(id);
+    this.setState({clicked: clicked});
+    console.log(this.state.clicked);
+
+    this.randomizeCharacters();
+  }
+
+  checkClicked = id => {
+    const clickCheck = this.state.clicked.filter(click => click === id);
+    console.log(clickCheck);
+
+    if (clickCheck.length === 0) {
+      this.clickedCard(id);
+      this.setState({score: this.state.score + 1});
+      this.setState({message: "You guessed correctly!"});
+    
+      if(this.state.score >= this.state.topScore) {
+        this.setState({topScore: this.state.score + 1});
+      }
+
+    } else {
+      this.setState({topScore: this.state.score});
+      this.setState({score: 0});
+      this.setState({clicked: []});
+      this.setState({message: "Game Over!"})
+    }
+
+    if (this.state.clicked.length === 12) {
+      this.setState({message: "You win!"})
+    }
+  }
+
+  render() {  
+    return (
+      <div className="container">
+        <TopBar 
+          score={this.state.score}
+          topScore={this.state.topScore}
+          message={this.state.message}
+        />
+        <Banner />
+        <GameBoard>
+            {this.state.characters.map(character => (
+            <Card 
+              id={character.id}
+              key={character.id}
+              name={character.name}
+              image={character.image}
+              checkClicked={this.checkClicked}
+            />
+          ))}
+        </GameBoard>
+      </div>
+    );
+  }
+
+}
+
+export default App;
